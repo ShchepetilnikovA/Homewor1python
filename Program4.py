3f4fd1617764acae5595316c742bfd8f7b149e9a
Shchepetilnikov <kalugalina.gugl@gmail.com>
2022-11-28 21:07:11 +0300

    program4 added

---
+++ Program4.py
@@ -0,0 +1,5 @@
+x=input('Введите номер четверти: ')
+if x == '1':print('x=[0,+oo];y=[0,+oo]')
+elif x == '2':print('x=[0,-oo];y=[0,+oo]')
+elif x == '3':print('x=[0,-oo];y=[0,-oo]')
+elif x == '4':print ('x=[0,+oo];y=[0,-oo]')
